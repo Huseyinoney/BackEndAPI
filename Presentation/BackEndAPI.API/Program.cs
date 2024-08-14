66406e38ed541bbdb66c74e3b7882c92f0d9d41d
Huseyinoney <hnsn1951@gmail.com>
2024-08-14 14:07:11 +0300

    jwt config for swagger

--- Presentation/BackEndAPI.API/Program.cs
+++ Presentation/BackEndAPI.API/Program.cs
@@ -1,6 +1,7 @@
 using BackEndAPI.Persistence;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 using System.Text;
 using static System.Net.WebRequestMethods;
@@ -13,7 +14,36 @@ builder.Services.AddControllers();
 builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EntityApi", Version = "v1", Description = "EntityApi Swagger Client" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Baerer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Bearer yazýp boþluk býrakýn ve jwt token ý yazýn"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+
+            },
+            Array.Empty<string>()
+        }
+
+    });
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
 {
