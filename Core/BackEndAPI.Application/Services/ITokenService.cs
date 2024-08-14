5162442c893af727510c441291566d1df292aed1
Huseyinoney <hnsn1951@gmail.com>
2024-08-14 15:06:52 +0300

    ITokenservice.cs updated

--- Core/BackEndAPI.Application/Services/ITokenService.cs
+++ Core/BackEndAPI.Application/Services/ITokenService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,8 @@ namespace BackEndAPI.Application.Services
     public interface ITokenService
     {
         public Token CreateToken(User user);
+
+        public ClaimsPrincipal ValidateToken(string token);
+        public string GetUsernameFromToken(string token);
     }
 }
